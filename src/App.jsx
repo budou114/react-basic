9932c1bab0e604e4dd6785abe2ecf771158b1910
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-21 09:41:24 +0900

    コンポーネントの再利用

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ function App() {
   return (
     <div>
       <Article
-        title={'新・日本一わかりやすいReact入門基礎編'}
+        title={'新・日本一わかりやすいReact入門基礎編4'}
         content={'今日のトピックはpropsのデータの受け渡しについて'}
+      />
+      <Article
+        title={'新・日本一わかりやすいReact入門基礎編5'}
+        content={'今日のトピックはuseStateについて'}
+      />
+      <Article
+        title={'新・日本一わかりやすいReact入門基礎編6'}
+        content={'今日のトピックはuseEffectについて'}
       />
     </div>
   );
