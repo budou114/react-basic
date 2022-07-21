6bb3636361512c37d08108fe5762666db10d42d7
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-21 09:36:08 +0900

    propsの使い方

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Article from "./components/Article";
+
+function App() {
+  return (
+    <div>
+      <Article
+        title={'新・日本一わかりやすいReact入門'}
+        content={'今日のトピックはpropsについて'}
+      />
+    </div>
+  );
+}
+
+export default App;
